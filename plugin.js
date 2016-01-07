f6111f2cb892ba31a162041931fdb565006dbe1f
Marcin Puhacz <marcin.puhacz@dataspin.io>
2016-01-07 20:34:42 +0100

    indent

--- plugin.js
+++ plugin.js
@@ -1,27 +1,26 @@
-(function ( $ ) {
- 
-    $.fn.delayedFunc = function( options ) {
- 
+(function($) {
+
+    $.fn.delayedFunc = function(options) {
+
         var settings = $.extend({
             selector: null,
             func: 'fadeIn',
             settings: {},
             delay: 300,
-        }, options );
+        }, options);
 
-        if(typeof($.fn[settings.func]) !== 'function') {
+        if (typeof($.fn[settings.func]) !== 'function') {
             throw '"' + settings.func + '" is not valid jQuery function.';
         }
- 
-        var o = !settings.selector ? this.children()  : this.find(settings.selector);
+
+        var o = !settings.selector ? this.children() : this.find(settings.selector);
 
         var delay = 0;
-        return o.each(function(){
+        return o.each(function() {
             $(this).delay(delay)[settings.func](settings.settings);
             delay += settings.delay;
         });
 
- 
     };
- 
-}( jQuery ));
+
+}(jQuery));
