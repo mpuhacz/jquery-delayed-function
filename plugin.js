d4e6c9186a8294abcf5ad446f780e9a5ae93bf8c
Marcin Puhacz <marcin.puhacz@dataspin.io>
2016-01-07 20:10:55 +0100

    initial commit

---
+++ plugin.js
@@ -0,0 +1,27 @@
+(function ( $ ) {
+ 
+    $.fn.delayedFunc = function( options ) {
+ 
+        var settings = $.extend({
+            selector: null,
+            func: 'fadeIn',
+            settings: {},
+            delay: 300,
+        }, options );
+
+        if(typeof($.fn[settings.func]) !== 'function') {
+            throw '"' + settings.func + '" is not valid jQuery function.';
+        }
+ 
+        var o = !settings.selector ? this.children()  : this.find(settings.selector);
+
+        var delay = 0;
+        return o.each(function(){
+            $(this).delay(delay)[settings.func](settings.settings);
+            delay += settings.delay;
+        });
+
+ 
+    };
+ 
+}( jQuery ));
